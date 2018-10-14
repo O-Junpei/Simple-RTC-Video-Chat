564d510f8a591d28c723f125edf85808596f5094
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2018-10-14 12:32:25 +0900

    server 部分を実装

--- server/signaling.js
+++ server/signaling.js
@@ -1,38 +1,26 @@
 "use strict";
-const wsUrl = 'ws://localhost:3001/';
-const ws = new WebSocket(wsUrl);
-ws.onopen = (evt) => {
-    console.log('ws open()');
-};
-ws.onerror = (err) => {
-    console.error('ws onerror() ERR:', err);
-};
-ws.onmessage = (evt) => {
-    console.log('ws onmessage() data:', evt.data);
-    const message = JSON.parse(evt.data);
-    switch(message.type){
-        case 'offer': {
-            console.log('Received offer ...');
-            textToReceiveSdp.value = message.sdp;
-            setOffer(message);
-            break;
-        }
-        case 'answer': {
-            console.log('Received answer ...');
-            textToReceiveSdp.value = message.sdp;
-            setAnswer(message);
-            break;
-        }
-        case 'candidate': {
-            console.log('Received ICE candidate ...');
-            const candidate = new RTCIceCandidate(message.ice);
-            console.log(candidate);
-            addIceCandidate(candidate);
-            break;
-        }
-        default: {
-            console.log("Invalid message");
-            break;
-         }
-    }
-};
+
+const WebSocketServer = require('ws').Server;
+const port = 3001;
+const wsServer = new WebSocketServer({ port: port });
+
+wsServer.on('connection', function(ws) {
+    console.log('-- websocket connected --');
+    ws.on('message', function(message) {
+        wsServer.clients.forEach(function each(client) {
+            if (isSame(ws, client)) {
+                console.log('- skip sender -');
+            }
+            else {
+                client.send(message);
+            }
+        });
+    });
+});
+
+function isSame(ws1, ws2) {
+    // -- compare object --
+    return (ws1 === ws2);
+}
+
+console.log('websocket server start. port=' + port);
